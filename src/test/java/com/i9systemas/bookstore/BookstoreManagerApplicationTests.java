8d639755b862e8ac9e8318dc8a519fa7b31884f0
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2025-10-22 11:05:48 -0300

    initialize Spring Boot project with Maven setup and basic configuration

---
+++ src/test/java/com/i9systemas/bookstore/BookstoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i9systemas.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
